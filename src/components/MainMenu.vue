9bb1f94a4882e297803877bb8a3a1ad7ad289eb0
Daniel Flanagan <daniel@lytedev.io>
2017-01-26 14:39:50 -0600

    main menuy styles

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,11 +1,27 @@
 <template lang="pug">
 .main-menu
-  router-link.btn(to="/characters") Characters
-  router-link.btn(to="/classes") Classes
+  router-link(to="/characters") Characters
+  router-link(to="/classes") Classes
 </template>
 
 <script>
 </script>
 
 <style lang="stylus">
+.main-menu
+  display flex
+  min-width 300px
+  flex-wrap wrap
+  justify-content space-around
+  align-items center
+  min-height 100vh
+  background #222
+
+  a
+    display flex
+    justify-content center
+    align-items center
+    background #333
+    width 50%
+    padding 1em
 </style>
